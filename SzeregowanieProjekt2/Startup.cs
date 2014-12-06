a5ce9fd770fa06dd80f876f8d937fe137b1fe3b5
dannybizarri <dannybizarri@gmail.com>
2014-12-06 16:58:59 +0100

    init commit

---
+++ SzeregowanieProjekt2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SzeregowanieProjekt2.Startup))]
+namespace SzeregowanieProjekt2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
